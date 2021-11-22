035b06a727b1a2b7e619190637729d0a2599050d
Marco <marco.apple180@gmail.com>
2021-11-21 21:10:04 -0600

    updates to layout

--- gwf_hub/overview/views.py
+++ gwf_hub/overview/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def overview(response):
-    return render(response, "base.html", {})
+    return render(response, "overview.html", {})
 
 def database(response):
     return HttpResponse("<h1>this is GWF Database</h1>")
