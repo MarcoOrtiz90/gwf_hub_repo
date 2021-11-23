d3459932822278b47779dd18013e2eb8fa531e55
Marco <marco.apple180@gmail.com>
2021-11-22 18:33:25 -0600

    changes made in root directory & updates in UI

--- gwf_hub/overview/views.py
+++ gwf_hub/overview/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def overview(response):
-    return render(response, "overview.html", {})
+    name = "GWF App Dashboard"    
+    return render(response, "overview.html", {'name':name})
 
 def database(response):
     return HttpResponse("<h1>this is GWF Database</h1>")
