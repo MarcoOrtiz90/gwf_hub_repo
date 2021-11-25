d2fb1790ec58d77a378e98b07f1ef80d886a297a
Marco <marco.apple180@gmail.com>
2021-11-25 14:48:14 -0600

    created auto answer generator app

--- gwf_hub/overview/urls.py
+++ gwf_hub/overview/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.overview, name="overview"),
     path('validator/', include('validator.urls')),
-#     path("validator/", views.validator_fun, name="validator"),
-      path("gwf-db/", views.database, name="gwf-db"),
-      path("workflow-hierarchy/", views.hierarchy, name="workflow-hierarchy"),
+    path('auto-answer-generator/', include('auto_answer_generator.urls')),
+    path("gwf-db/", views.database, name="gwf-db"),
+    path("workflow-hierarchy/", views.hierarchy, name="workflow-hierarchy"),
  ]
