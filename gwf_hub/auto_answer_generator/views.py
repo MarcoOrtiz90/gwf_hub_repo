c6b2aabce0161e7ec5158ed1ea1eb20c9b27a0b4
Marco <marco.apple180@gmail.com>
2021-11-27 16:54:25 -0600

    updated auto answers database and generator.html

--- gwf_hub/auto_answer_generator/views.py
+++ gwf_hub/auto_answer_generator/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import AutoAnswerQuestionTable, AutoAnswersTable
 
 # Create your views here.
 def aaGenerator(request):
-    return render(request, "generator.html", {})
+    question_ids_table = AutoAnswerQuestionTable.objects.all()
+    context = {'question_ids_table': question_ids_table}
+    return render(request, "generator.html", context)
