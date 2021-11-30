952d20bd97e4b55ab760f6473effe81f673ab077
Marco <marco.apple180@gmail.com>
2021-11-30 12:41:43 -0600

    generator updates on form

--- gwf_hub/auto_answer_generator/views.py
+++ gwf_hub/auto_answer_generator/views.py
@@ -4,15 +4,19 @@ from .models import AutoAnswerQuestionTable, AutoAnswersTable
 # Create your views here.
 def aaGenerator(request):
     question_ids_table = AutoAnswerQuestionTable.objects.all()
-    context = {'question_ids_table': question_ids_table}
-    
     if request.method == "POST":
-        if request.POST.get("new-id"):
-            pass
-        elif request.POST.get("request"):
-            if request.htmx:
-                content = request.POST.getlist('question_ids', '')
-                print(content[1])
-                return render(request, 'generator-form.html', context)
-    
+        context2 = request.POST.getlist('question_ids', '')
+        content = {
+                'question_ids_table': question_ids_table,
+                'prevSelect': context2
+        }
+        print(content)
+        print("second")
+        return render(request, 'generator.html', content)
+
+    context = {'question_ids_table': question_ids_table}
+    print(context)
+    print("first")
     return render(request, "generator.html", context)
+
+    
