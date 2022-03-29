0cdddfcd086966e0c6271accc89f8b828aa44da4
Marco <marco.apple180@gmail.com>
2022-03-29 12:13:59 -0600

    modified function access

--- gwf_hub/accounts/views.py
+++ gwf_hub/accounts/views.py
@@ -9,8 +9,8 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def login_view(request):    
-    if request.user.is_authenticated:
-        return redirect('overview')
+    # if request.user.is_authenticated:
+    return redirect('overview')
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
