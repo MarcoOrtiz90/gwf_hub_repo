7e5d86bef797a46ef4d2282d51039d598ea30865
Marco <marco.apple180@gmail.com>
2022-07-11 14:19:12 -0600

    restored venv structure, re-added new interface files

---
+++ gwf_hub/payload_builder/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+def payloadBuilder(request):
+    if request.method == 'post':
+        if request.POST.get('buildPayload'):
+            inputs = request.POST
+            iterationCount = inputs['builderIterations']
+            iteration = 1
+            categories = ['string', 'step', 'answer']
+            currentMain = ''
+            export = ''
+            while iteration <= iterationCount:                
+                for category in categories:                    
+                    if category == "string" :
+                        loopData = ''
+
+    return render(request, 'builder.html')
